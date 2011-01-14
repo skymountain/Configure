98c347a5d844462b5c219bd92d6e812aeacbbf68
Taro Sekiyama <t-sekiym@kuis.kyoto-u.ac.jp>
2011-01-15 03:51:08 +0900

    enable to pass command name and initial directory as parameters

--- bin/configure.pl
+++ bin/configure.pl
@@ -8,7 +8,14 @@ use FindBin;
 use lib file($FindBin::Bin)->parent->subdir('lib')->stringify;
 
 use Configure::Command;
+use Configure::Print;
 
+my $comm_name = $ARGV[0];
+my $init_dir  = $ARGV[1];
 
+unless ($comm_name && $init_dir) {
+  Configure::Print->error("you must specify command name and initial directory");
+  exit;
+}
 
-Configure::Command->new('install')->execute_from_config($ENV{DOTDIR});
+Configure::Command->new($comm_name)->execute_from_config($init_dir);
